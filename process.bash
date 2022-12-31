52c2d8339c8fe20f9780fcf6c2a943ec8af12c0f
KAndHisC <shichuan@ualberta.ca>
2022-12-31 23:53:48 +0000

    add new model training

--- process.bash
+++ process.bash
@@ -4,7 +4,7 @@
 #                       --seq_length 512 --target lm
 
 
-python3 preprocess.py --corpus_path corpora/clyric/target/poem.txt \
-                      --vocab_path models/google_zh_vocab.txt \
-                      --dataset_path corpora/clyric/processed/lyric_dataset.pt --processes_num 16 \
+python3 preprocess.py --corpus_path corpora/clyric/target/poem_256.txt \
+                      --vocab_path models/my_vocab.txt \
+                      --dataset_path corpora/clyric/processed/poem_256_dataset.pt --processes_num 16 \
                       --seq_length 256 --data_processor lm
