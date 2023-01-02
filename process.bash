6a48c9841dff77e34a7f19be123e0969120a4872
KAndHisC <shichuan@ualberta.ca>
2023-01-02 01:54:07 +0000

    change path

--- process.bash
+++ process.bash
@@ -4,7 +4,7 @@
 #                       --seq_length 512 --target lm
 
 
-python3 preprocess.py --corpus_path myspace/modern-poem-dataset/target/poem_256.txt \
+python3 preprocess.py --corpus_path myspace/modern-poem-dataset/processed/poem_256.txt \
                       --vocab_path myspace/my_vocab.txt \
                       --dataset_path myspace/modern-poem-dataset/processed/poem_256_dataset.pt --processes_num 16 \
                       --seq_length 256 --data_processor lm
