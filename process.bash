3ea16d62fc35318359e83bb79cdd8ec3ed5bc45d
KandHisC <shichuan@ualberta.ca>
2023-03-04 14:20:07 +0800

    change dir

--- process.bash
+++ process.bash
@@ -4,7 +4,7 @@
 #                       --seq_length 512 --target lm
 
 
-python3 preprocess.py --corpus_path myspace/modern-poem-dataset/processed/poem_256.txt \
-                      --vocab_path myspace/my_vocab.txt \
-                      --dataset_path myspace/modern-poem-dataset/processed/poem_256_dataset.pt --processes_num 16 \
+python3 preprocess.py --corpus_path datasets/poems/all_poems.txt \
+                      --vocab_path models/my_vocab.txt \
+                      --dataset_path datasets/poems/poem_256_dataset.pt --processes_num 16 \
                       --seq_length 256 --data_processor lm
