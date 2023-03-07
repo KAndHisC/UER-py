43621febee98d21b67a1a3770298e3094bbdbf7f
KAndHisC <shichuan@ualberta.ca>
2023-03-07 07:40:17 +0000

    add cpm

---
+++ process_cpm.bash
@@ -0,0 +1,10 @@
+# python preprocess.py --corpus_path corpora/lyric.txt \
+#                       --vocab_path models/lyric/vocab.txt \
+#                       --dataset_path lyric_dataset.pt --processes_num 16 \
+#                       --seq_length 512 --target lm
+
+
+python3 preprocess.py --corpus_path datasets/poems/poems_data.txt \
+                      --vocab_path models/my_vocab.txt \
+                      --dataset_path datasets/poems/poem_256_dataset.pt --processes_num 16 \
+                      --seq_length 256 --data_processor lm
---
+++ train-cpm.bash
@@ -0,0 +1,20 @@
+# python pretrain.py --dataset_path lyric_dataset.pt \
+#                     --pretrained_model_path models/clyric/lyric_model.bin \
+#                     --vocab_path models/clyric/vocab.txt \
+#                     --config_path models/clyric/config.json \
+#                     --output_model_path models/clyric/lyric_gpt2_model.bin \
+#                     --world_size 1 --gpu_ranks 0 \
+#                     --total_steps 100000 --save_checkpoint_steps 10000 --report_steps 100 \
+#                     --learning_rate 5e-5 --batch_size 8 \
+#                     --embedding word_pos --remove_embedding_layernorm \
+#                     --encoder transformer --mask causal --layernorm_positioning pre \
+#                     --target lm --tie_weights
+
+python3 pretrain.py --dataset_path datasets/poems/poem_256_dataset.pt \
+                    --pretrained_model_path models/clue/clue-gpt-uer.bin \
+                    --vocab_path models/my_vocab.txt \
+                    --config_path models/gpt-config-1024.json \
+                    --output_model_path models/clue/clue.bin \
+                    --world_size 1 --gpu_ranks 0 \
+                    --total_steps 100000 --save_checkpoint_steps 10000 --report_steps 1000 \
+                    --learning_rate 5e-5 --batch_size 32
