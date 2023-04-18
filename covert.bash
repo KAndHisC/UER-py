1b85b8b05f9a33570d6074a9013bd04b9ad86189
takiwang <shichuan@ualberta.ca>
2023-04-18 11:47:08 +0000

    cpm

--- covert.bash
+++ covert.bash
@@ -1,3 +1,3 @@
-python scripts/convert_gpt2_from_uer_to_huggingface.py --input_model_path ./models/clue/clue.bin-200000 \
+python scripts/convert_gpt2_from_uer_to_huggingface.py --input_model_path ./models/clue/clue.bin-40000 \
                                                         --output_model_path ./models/clue/pytorch_model.bin \
                                                         --layers_num 12
